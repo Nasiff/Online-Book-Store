d68491dd3fde0066ba4b72fca92d7e9d684e943f
Dennis Phetsomphou <dennisph@my.yorku.ca>
2020-11-20 19:22:33 -0500

    Routing Setup

--- book-store-app/src/App.js
+++ book-store-app/src/App.js
@@ -1,13 +1,44 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import MainScreen from './Components/MainScreen.jsx'
+import CartScreen from './Components/CartScreen.jsx'
 
-function App() {
-  return (
-    <div className="App">
-      <MainScreen/>
-    </div>
-  );
+import { BrowserRouter as Router,
+Switch,
+Route,
+Link,
+useRouteMatch,
+useParams } from "react-router-dom";
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cart: []
+    };
+  }
+
+
+  handleAddToCart = (id, name, qty, price) => {
+    console.log("Adding to cart: " + id);
+  }
+
+  render(){
+    return (
+      <Router>
+          <Switch>
+            <Route path="/cart">
+              <CartScreen cart={this.state.cart}/>
+            </Route>
+            <Route path="/">
+              <MainScreen addToCart={this.handleAddToCart}/>
+            </Route>
+          </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
