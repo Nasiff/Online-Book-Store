7193a1952c664a798229f7a63e5abc141c42e8b3
Dennis Phetsomphou <dennisph@my.yorku.ca>
2020-11-21 19:44:05 -0500

    Cart Link

--- book-store-app/src/Components/BrowseHeader.jsx
+++ book-store-app/src/Components/BrowseHeader.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import cart from '../Assets/cart.png'
-
+import { BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams } from "react-router-dom";
+    
 class BrowseHeader extends React.Component {
     va
     render() {
@@ -18,10 +24,17 @@ class BrowseHeader extends React.Component {
                     <li className="category grow">Romance</li>
                     <li className="category grow">Fantasy</li>
                 </ul>
-            <div className="inline growX" id="cart">
-                <img style={styles.cart} src={cart} alt="Logo"></img>
-                <h3>Cart</h3>
-            </div>
+            <Link className="lg-view" style={{padding: "0px"}}to="/cart">
+                <div className="inline growX" id="cart">
+                    <img style={styles.cart} src={cart} alt="Logo"></img>
+                    <h3>Cart</h3>
+                </div>
+            </Link>
+            <Link className="sm-view" style={{padding: "0px"}}to="/cart">
+                <div className="inline growX" id="smallCart">
+                    <img style={styles.smallCart} src={cart} alt="Logo"></img>
+                </div>
+            </Link>
         </div> 
         );
     }
@@ -31,6 +44,11 @@ const styles = {
     cart: {
         height: "35%",
         margin: "5px",
+        overflow: "hidden"
+    },
+    smallCart: {
+        height: "35%",
+        margin: "0px",
         overflow: "hidden"
     }
 }
