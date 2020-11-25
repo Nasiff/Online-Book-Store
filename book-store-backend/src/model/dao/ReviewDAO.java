7a5157b7d387b7c401b7c55644b4b928bfa54292
Patrick Tan <tanpatri@my.yorku.ca>
2020-11-24 22:09:26 -0500

    Updated part of CREATE BookReview request
    Still work in progress.

--- book-store-backend/src/model/dao/ReviewDAO.java
+++ book-store-backend/src/model/dao/ReviewDAO.java
@@ -49,4 +49,69 @@ public class ReviewDAO {
 		con.close();
 		return reviews;
 	}
+	
+	public ReviewBean retrieveBookReviewByBidAndUid(String bid, int uid) throws SQLException {
+		String query = "select * from Book join BookReview on Book.bid = BookReview.bid where Book.bid = ? and BookReview.uid = ?";
+		
+		Connection con = this.ds.getConnection();
+		PreparedStatement p = con.prepareStatement(query);
+		
+		p.setString(1, bid);
+		p.setInt(2, uid);
+		
+		ReviewBean reviewBean = null;
+		ResultSet r = p.executeQuery();
+		while (r.next()) {
+			String rid = r.getString("rid");
+			String review = r.getString("review");
+			int score = r.getInt("score");
+			reviewBean = new ReviewBean(rid, bid, uid, review, score);
+		}
+		
+		r.close();
+		p.close();
+		con.close();
+		return reviewBean;
+	}
+	
+	public String getMaxReviewId() throws SQLException {
+		String query = "select max(rid) from BookReview";
+		Connection con = this.ds.getConnection();
+		PreparedStatement p = con.prepareStatement(query);
+		
+		String maxReviewId = null;
+		ResultSet r = p.executeQuery();
+		if (r.next()) {
+			System.out.println("maxReviewId: " + r.getString(1));
+			maxReviewId = r.getString(1);
+		}
+		
+		r.close();
+		p.close();
+		con.close();
+		return maxReviewId;
+	}
+	
+	public int insertBookReview(int uid, String lname, String fname, String email,
+			String password, String user_type) throws SQLException {
+		String query = "insert into UserAccount values(?,?,?,?,?,?)";
+		Connection con = this.ds.getConnection();
+		PreparedStatement p = con.prepareStatement(query);
+		p.setInt(1, uid);
+		p.setString(2, lname);
+		p.setString(3, fname);
+		p.setString(4, email);
+		p.setString(5, password);
+		p.setString(6, user_type);
+		
+		int rows = p.executeUpdate();
+		System.out.println("DB: Added " + rows + " user to the database");
+		
+		p.close();
+		con.close();
+		return rows;
+	}	
+	
+	
+	
 }
